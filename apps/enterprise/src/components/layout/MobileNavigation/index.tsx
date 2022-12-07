d41d386ba74a271bae2896e0548957d293ec6272
Rodion Chachura <geekrodion@gmail.com>
2022-12-07 17:05:26 +0400

    Update: mobile layout

--- apps/enterprise/src/components/layout/MobileNavigation/index.tsx
+++ apps/enterprise/src/components/layout/MobileNavigation/index.tsx
@@ -3,40 +3,22 @@ import { ManageWallet } from 'chain/components/ManageWallet';
 import { CreateDaoButton } from 'dao/components/CreateDaoButton';
 import { DashboardButton } from 'dao/components/DashboardButton';
 import { ComponentWithChildrenProps } from 'lib/shared/props';
-import { HStack, VStack } from 'lib/ui/Stack';
-import { getVerticalPaddingCSS } from 'lib/ui/utils/getVerticalPaddingCSS';
-import styled, { css } from 'styled-components';
+import { HStack } from 'lib/ui/Stack';
+import styled from 'styled-components';
 import { Favourites } from '../Favourites';
 
-const navigationHeight = 86;
-
-const Container = styled(VStack)`
+const Container = styled.div`
   height: 100%;
-  position: relative;
+  display: grid;
+  grid-template-rows: 86px 1fr 86px;
 `;
 
 const ContentWr = styled.div`
   overflow-y: auto;
 `;
 
-const Content = styled.div`
-  ${getVerticalPaddingCSS(navigationHeight)}
-`;
-
-const sharedNavigationSectionCSS = css`
-  position: absolute;
-  z-index: 1;
-  left: 0;
-
-  width: 100%;
-  height: ${getCSSUnit(navigationHeight)};
-
-  padding: 16px;
-`;
-
 const Header = styled(HStack)`
-  ${sharedNavigationSectionCSS};
-  top: 0;
+  padding: 16px;
   padding-top: 32px;
 
   border-bottom: 1px solid #242627;
@@ -44,11 +26,11 @@ const Header = styled(HStack)`
 `;
 
 const Footer = styled(HStack)`
-  ${sharedNavigationSectionCSS};
-  bottom: 0;
-  padding-bottom: 32px;
+  padding: 16px;
+  padding-bottom: 24px;
 
-  background: linear-gradient(180deg, rgba(21, 23, 24, 0) 0%, #151718 100%);
+  border-top: 1px solid #242627;
+  background: rgba(21, 23, 24, 0.9);
 `;
 
 export const MobileNavigation = ({ children }: ComponentWithChildrenProps) => {
@@ -58,9 +40,7 @@ export const MobileNavigation = ({ children }: ComponentWithChildrenProps) => {
         <div />
         <ManageWallet />
       </Header>
-      <ContentWr>
-        <Content>{children}</Content>
-      </ContentWr>
+      <ContentWr>{children}</ContentWr>
       <Footer alignItems="center" justifyContent="space-between">
         <HStack gap={16}>
           <DashboardButton />
