d1dc1605439516b4b6b9cdd661cf4075016793bc
Rodion Chachura <geekrodion@gmail.com>
2022-12-08 16:56:07 +0400

    responsive multisig members

--- apps/enterprise/src/pages/dao/members/MultisigMemberItem.tsx
+++ apps/enterprise/src/pages/dao/members/MultisigMemberItem.tsx
@@ -3,13 +3,15 @@ import { Container } from '@terra-money/apps/components';
 import { Address } from 'components/address';
 import { MultisigMember } from 'types/MultisigMember';
 import styles from './MultisigMemberItem.module.sass';
+import { useIsSmallScreen } from 'lib/ui/hooks/useIsSmallScreen';
 
 interface MultisigMemberItemProps extends MultisigMember {}
 
 export const MultisigMemberItem = ({ addr, weight }: MultisigMemberItemProps) => {
+  const isSmallScreen = useIsSmallScreen();
   return (
     <Container className={styles.root} direction="row">
-      <Address address={addr} truncation="none" />
+      <Address address={addr} truncation={isSmallScreen ? undefined : 'none'} />
       <Text variant="text">{weight}</Text>
     </Container>
   );
