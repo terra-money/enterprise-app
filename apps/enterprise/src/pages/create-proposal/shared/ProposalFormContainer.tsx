93bd01ba937ab6927c61137732e361f5e69d1d3d
Rodion Chachura <geekrodion@gmail.com>
2022-12-19 11:36:41 +0400

    fix: proposal form container

--- apps/enterprise/src/pages/create-proposal/shared/ProposalFormContainer.tsx
+++ apps/enterprise/src/pages/create-proposal/shared/ProposalFormContainer.tsx
@@ -1,9 +1,9 @@
-import { Container, Form, ScrollableContainer } from '@terra-money/apps/components';
 import { useRef } from 'react';
 import { Header } from '../Header';
-import { AnimatedPage, StickyHeader } from '@terra-money/apps/components';
+import { AnimatedPage } from '@terra-money/apps/components';
 import { CreateProposalFooter } from '../CreateProposalFooter';
-import styles from './ProposalFormContainer.module.sass';
+import styled from 'styled-components';
+import { VStack } from 'lib/ui/Stack';
 
 interface ProposalFormContainerProps {
   title: string;
@@ -13,29 +13,32 @@ interface ProposalFormContainerProps {
   disabled: boolean;
 }
 
+const Container = styled(VStack)`
+  height: 100%;
+  width: 100%;
+  padding: 48px 48px 0 48px;
+  gap: 32px;
+
+  overflow-y: auto;
+`;
+
+const Content = styled.div`
+  flex: 1;
+  max-width: 600px;
+`;
+
 export const ProposalFormContainer = ({ title, children, loading, onSubmit, disabled }: ProposalFormContainerProps) => {
   const ref = useRef<HTMLDivElement>(null);
 
   return (
-    <div className={styles.root}>
-      <ScrollableContainer
-        stickyRef={ref}
-        header={(visible) => (
-          <StickyHeader visible={visible}>
-            <Header compact={true} title={title} />
-          </StickyHeader>
-        )}
-      >
-        <AnimatedPage className={styles.content}>
-          <Header ref={ref} title={title} />
-          <Form className={styles.form}>
-            <Container className={styles.container} gap={32} direction="column">
-              {children}
-            </Container>
-          </Form>
-          <CreateProposalFooter loading={loading} onSubmit={onSubmit} disabled={disabled} />
-        </AnimatedPage>
-      </ScrollableContainer>
-    </div>
+    <AnimatedPage>
+      <Container>
+        <Header ref={ref} title={title} />
+        <Content>
+          <VStack gap={32}>{children}</VStack>
+        </Content>
+        <CreateProposalFooter loading={loading} onSubmit={onSubmit} disabled={disabled} />
+      </Container>
+    </AnimatedPage>
   );
 };
