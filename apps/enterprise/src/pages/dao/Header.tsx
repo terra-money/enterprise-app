b772fd65d9e80b08c5f11c511479f8942e996b12
Rodion Chachura <geekrodion@gmail.com>
2022-12-22 10:31:43 +0400

    fix: dao description location

--- apps/enterprise/src/pages/dao/Header.tsx
+++ apps/enterprise/src/pages/dao/Header.tsx
@@ -3,6 +3,8 @@ import classNames from 'classnames';
 import { DAOLogo } from 'components/dao-logo';
 import { FavouriteToggle } from 'components/favourite-toggle';
 import { Text } from 'components/primitives';
+import { Line } from 'lib/ui/Line';
+import { VStack } from 'lib/ui/Stack';
 import { useCurrentDao } from 'pages/shared/CurrentDaoProvider';
 import { forwardRef, Ref } from 'react';
 import { useNavigate } from 'react-router';
@@ -37,17 +39,20 @@ export const Header = forwardRef((props: HeaderProps, ref: Ref<HTMLDivElement>) 
 
   return (
     <Container className={classNames(className, styles.root)} direction="column">
-      <Container className={styles.container}>
-        <Text className={styles.back} variant="link" onClick={() => navigate(-1)}>
-          Back
-        </Text>
-        <DAOLogo className={styles.logo} logo={dao.logo} />
-        <FavouriteToggle className={styles.favourite} dao={dao} />
-        <Text className={styles.name} variant="heading2">
-          {dao.name}
-        </Text>
+      <VStack gap={16}>
+        <Container className={styles.container}>
+          <Text className={styles.back} variant="link" onClick={() => navigate(-1)}>
+            Back
+          </Text>
+          <DAOLogo className={styles.logo} logo={dao.logo} />
+          <FavouriteToggle className={styles.favourite} dao={dao} />
+          <Text className={styles.name} variant="heading2">
+            {dao.name}
+          </Text>
+        </Container>
         {dao.description && <Text variant="text">{dao.description}</Text>}
-      </Container>
+        <Line />
+      </VStack>
       <Container ref={ref} className={styles.tabs}>
         <DaoNavigation />
       </Container>
