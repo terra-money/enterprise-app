e42e0699eeeb410e5f295b0b2fe36e0b17290637
Rodion Chachura <geekrodion@gmail.com>
2022-12-22 09:57:34 +0400

    show token name in the dropdown

--- apps/enterprise/src/pages/create-proposal/spend/TreasuryTokenInput.tsx
+++ apps/enterprise/src/pages/create-proposal/spend/TreasuryTokenInput.tsx
@@ -1,4 +1,9 @@
+import { SafeImage } from 'components/safe-image/SafeImage';
+import { CoverImage } from 'lib/ui/images/CoverImage';
+import { ImageHolder } from 'lib/ui/images/ImageHolder';
 import { FixedOptionsInput } from 'lib/ui/inputs/Combobox/FixedOptionsInput';
+import { HStack, VStack } from 'lib/ui/Stack';
+import { Text } from 'lib/ui/Text';
 import { TreasuryToken } from 'queries';
 import { useCurrentDaoTreasuryTokens } from './CurrentDAOTreasuryTokentsProvider';
 
@@ -20,6 +25,19 @@ export const TreasuryTokenInput = ({ value, onChange, error }: TreasuryTokenInpu
       onChange={onChange}
       optionToString={(token) => token.key}
       value={value}
+      renderOption={(token) => (
+        <HStack alignItems="center" gap={8}>
+          <ImageHolder width={32} height={32}>
+            <SafeImage src={token.icon} render={(props) => <CoverImage {...props} />} />
+          </ImageHolder>
+          <VStack>
+            <Text>{token.name || token.symbol}</Text>
+            <Text size={14} cropped color="supporting">
+              {token.key}
+            </Text>
+          </VStack>
+        </HStack>
+      )}
     />
   );
 };
