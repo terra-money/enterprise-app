7215a774e3be8bc023b605a9fef0317c33de5bbb
Rodion Chachura <geekrodion@gmail.com>
2023-01-26 17:26:23 +0400

    hotfix: checkbox

--- apps/enterprise/src/lib/ui/inputs/Checkbox/InvisibleHTMLCheckbox.tsx
+++ apps/enterprise/src/lib/ui/inputs/Checkbox/InvisibleHTMLCheckbox.tsx
@@ -1,8 +1,8 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export interface InvisibleHTMLCheckboxProps {
-  value: boolean
-  onChange: (value: boolean) => void
+  value: boolean;
+  onChange: (value: boolean) => void;
 }
 
 const CheckboxInput = styled.input`
@@ -12,19 +12,15 @@ const CheckboxInput = styled.input`
   margin: -1px;
   overflow: hidden;
   padding: 0;
-  position: absolute;
   width: 1px;
-`
+`;
 
-export const InvisibleHTMLCheckbox = ({
-  value,
-  onChange,
-}: InvisibleHTMLCheckboxProps) => (
+export const InvisibleHTMLCheckbox = ({ value, onChange }: InvisibleHTMLCheckboxProps) => (
   <CheckboxInput
     type="checkbox"
     checked={value}
     onChange={(event) => {
-      onChange(event.currentTarget.checked)
+      onChange(event.currentTarget.checked);
     }}
   />
-)
+);
