ce4fee010a818f003b593714f528efeaa6276e5f
Terence Lim <terencelimz@gmail.com>
2023-02-15 19:43:04 +0800

    Fix issue whereby execute proposals does not parse array of messages (#36)

--- apps/enterprise/src/pages/create-proposal/execute/helpers/toExecuteMsg.ts
+++ apps/enterprise/src/pages/create-proposal/execute/helpers/toExecuteMsg.ts
@@ -20,17 +20,21 @@ export const base64encode = (input: string): string => {
   return Buffer.from(JSON.stringify(JSON.parse(JSON.stringify(input)))).toString('base64');
 };
 
-export const toExecuteMsg = (value: string): string => {
+export const toExecuteMsg = (value: string): string[] => {
   const result: ExecuteMsgInput = JSON.parse(value);
 
-  if (result.wasm) {
-    encodedWasmFields.forEach((fieldName) => {
-      const field = result.wasm?.[fieldName];
-      if (field) {
-        field.msg = base64encode(field.msg);
-      }
-    });
-  }
-
-  return JSON.stringify(result);
+  const messages = Array.isArray(result) ? result : [result]
+  
+  messages.forEach((message) => {
+    if (message.wasm) {
+      encodedWasmFields.forEach((fieldName) => {
+        const field = message.wasm?.[fieldName];
+        if (field) {
+          field.msg = base64encode(field.msg);
+        }
+      });
+    }
+  })
+  
+  return messages.map((msg) => JSON.stringify(msg));
 };
