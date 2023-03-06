1e5047b5eb4b85198e57b89175d6436da1833da2
Rodion Chachura <geekrodion@gmail.com>
2023-03-06 21:24:59 +0400

    feat: styled proposals filter

--- apps/enterprise/src/pages/dao/proposals/ProposalsPage.tsx
+++ apps/enterprise/src/pages/dao/proposals/ProposalsPage.tsx
@@ -5,7 +5,7 @@ import { useVotingPowerQuery } from 'queries';
 import { ProposalCard } from '../../shared/ProposalCard';
 import { useConnectedWallet } from '@terra-money/wallet-provider';
 import Big from 'big.js';
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useCurrentDao } from 'dao/components/CurrentDaoProvider';
 import { HStack } from 'lib/ui/Stack';
 import { ResponsiveView } from 'lib/ui/ResponsiveView';
@@ -14,9 +14,9 @@ import { useDaoProposalsQuery } from 'queries/useDaoProposalsQuery';
 import { PrimaryButton } from 'lib/ui/buttons/rect/PrimaryButton';
 import { EmptyStatePlaceholder } from 'lib/ui/EmptyStatePlaceholder';
 import { InternalLink } from 'components/link';
-import styles from './ProposalsPage.module.sass';
-import { Text } from 'lib/ui/Text';
-import { Divider } from '@mui/material';
+import { enterprise } from 'types/contracts';
+import { proposalStatuses } from 'proposal';
+import { ProposalsFilter } from './ProposalsFilter';
 
 const LIMIT = 100;
 
@@ -33,55 +33,13 @@ export const ProposalsPage = () => {
     input: '',
     searchText: '',
   });
-
-  const proposalStatuses = [
-    { label: 'Pending', value: 'in_progress' },
-    { label: 'Executed', value: 'executed' },
-    { label: 'Passed', value: 'passed' },
-    { label: 'Failed', value: 'rejected' },
-  ];
-
-  const [showDropdown, setShowDropdown] = useState(false);
-  const [selectedStatusFilter, setSelectedStatusFilter] = useState<string>('');
-  const dropdownRef = useRef<HTMLDivElement>(null);
-  const handleToggleDropdown = () => {
-    setShowDropdown(!showDropdown);
-  };
-
-  useEffect(() => {
-    if (showDropdown) {
-      const handleClick = (event: MouseEvent) => {
-        if (!dropdownRef.current || !dropdownRef.current.contains(event.target as Node)) {
-          setShowDropdown(false);
-        }
-      };
-      document.addEventListener('mousedown', handleClick);
-      return () => {
-        document.removeEventListener('mousedown', handleClick);
-      };
-    }
-  }, [showDropdown]);
+  const [statusesToDisplay, setStatusesToDisplay] = useState<enterprise.ProposalStatus[]>(proposalStatuses);
 
   const proposals = useMemo(() => {
     return proposalsQuery?.filter((proposal) => {
       return proposal.title.toLowerCase().includes(search.searchText);
-    });
-  }, [proposalsQuery, search.searchText]);
-
-  const filteredProposals = useMemo(() => {
-    if (!selectedStatusFilter) {
-      return proposals;
-    }
-    return proposals?.filter((item) => item.status === selectedStatusFilter);
-  }, [proposals, selectedStatusFilter]);
-
-  const handleStatusFilterChange = (event: any) => {
-    if (event.target.value === selectedStatusFilter) {
-      setSelectedStatusFilter('');
-    } else {
-      setSelectedStatusFilter(event.target.value);
-    }
-  };
+    }).filter(({ status }) => statusesToDisplay.includes(status));
+  }, [proposalsQuery, search.searchText, statusesToDisplay]);
 
   const navigate = useNavigate();
 
@@ -119,43 +77,10 @@ export const ProposalsPage = () => {
             }
           />
 
-          <PrimaryButton
-            className={styles.filterButton}
-            as="div"
-            kind="secondary"
-            onClick={() => {
-              handleToggleDropdown();
-            }}
-          >
-            <ResponsiveView small={() => 'Filter'} normal={() => 'Filter'} />
-          </PrimaryButton>
-          <Container ref={dropdownRef}>
-            {showDropdown && (
-              <div ref={dropdownRef}>
-                <Container className={styles.filterContainer} direction="column">
-                  <Text className={styles.filterLabel}>Proposal Status</Text>
-                  <Container direction='column' gap={10}>
-                    {proposalStatuses.map((filter) => (
-                      <div key={filter.value} className={styles.filterOption}>
-                        <Container direction="row" gap={10}>
-                          <input
-                            type="radio"
-                            name="filter"
-                            value={filter.value}
-                            checked={filter.value === selectedStatusFilter}
-                            onChange={handleStatusFilterChange}
-                          />
-                          <label>{filter.label}</label>
-                        </Container>
-                      </div>
-                    ))}
-                  </Container>
-                  <Divider></Divider>
-                  <Text className={styles.resetButton} onClick={() => setSelectedStatusFilter('')}>Reset all filters</Text>
-                </Container>
-              </div>
-            )}
-          </Container>
+          <ProposalsFilter
+            value={statusesToDisplay}
+            onChange={setStatusesToDisplay}
+          />
         </Container>
         <PrimaryButton
           as="div"
@@ -191,7 +116,7 @@ export const ProposalsPage = () => {
             />
           )
         ) : (
-          filteredProposals?.map((proposal, index) => (
+          proposals?.map((proposal, index) => (
             <ProposalCard key={index} variant="extended" proposal={proposal} />
           ))
         )}
