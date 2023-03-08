d6e6147bc4a943cdd618641873c9ee888c5a57a0
Rodion Chachura <geekrodion@gmail.com>
2023-03-08 16:56:22 +0400

    update: allow any input for social field

--- apps/enterprise/src/pages/create-dao/shared/helpers/validateSocials.ts
+++ apps/enterprise/src/pages/create-dao/shared/helpers/validateSocials.ts
@@ -1,28 +1,45 @@
 import { FormState } from '@terra-money/apps/hooks';
 import { DaoSocialDataInput } from 'pages/create-dao/DaoWizardFormProvider';
 
+// accepts anything
+const validateSocial = (value: string | undefined) => {
+  return undefined
+}
+
 export const validateGithubUsername = (value: string | undefined) => {
-  if (value && /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i.test(value) === false) {
-    return `Invalid Github Username`;
-  }
+  return validateSocial(value)
+
+  // GitHub username validadion
+  // if (value && /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i.test(value) === false) {
+  //   return `Invalid Github Username`;
+  // }
 };
 
 export const validateTwitterUsername = (value: string | undefined) => {
-  if (value && /^@?(\w){1,15}$/.test(value) === false) {
-    return `Invalid Twitter Username`;
-  }
+  return validateSocial(value)
+
+  // Twitter username validadion
+  // if (value && /^@?(\w){1,15}$/.test(value) === false) {
+  //   return `Invalid Twitter Username`;
+  // }
 };
 
 export const validateDiscordUsername = (value: string | undefined) => {
-  if (value && /^.{3,32}#[0-9]{4}$/.test(value) === false) {
-    return `Invalid Discord Username`;
-  }
+  return validateSocial(value)
+
+  // Discord username validadion
+  // if (value && /^.{3,32}#[0-9]{4}$/.test(value) === false) {
+  //   return `Invalid Discord Username`;
+  // }
 };
 
 export const validateTelegramUsername = (value: string | undefined) => {
-  if (value && /^(https:\/\/t\.me\/|t\.me\/|@|)(\w+)$/.test(value) === false) {
-    return `Invalid Telegram Username`;
-  }
+  return validateSocial(value)
+
+  // Telegram username validadion
+  // if (value && /^(https:\/\/t\.me\/|t\.me\/|@|)(\w+)$/.test(value) === false) {
+  //   return `Invalid Telegram Username`;
+  // }
 };
 
 export const validateSocials = ({
