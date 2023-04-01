8f25b829a4705ca77ece312ee62f8cce20499fef
NikolaCehic95 <nikola95cehic@gmail.com>
2023-04-02 01:18:33 +0200

    fix: create telegram url

--- apps/enterprise/src/utils/createTelegramURL.ts
+++ apps/enterprise/src/utils/createTelegramURL.ts
@@ -1,12 +1,9 @@
 export const createTelegramURL = (username: string) => {
-  if (username.startsWith("https://t.me/")) {
-    return `https://t.me/${username}`;
-  }
   if (username.startsWith("t.me/")) {
     return `t.me/${username}`;
   }
   if (username.startsWith("@")) {
     return `@${username}`;
   }
-  return `${username}`;
+  return `https://t.me/${username}`;
 };
