58e6d8c937a29d40211bec323f2b024b910ca5c7
NikolaCehic95 <nikola95cehic@gmail.com>
2023-04-17 14:28:11 +0200

    fix: add a toggle for base64/decoded wasm msg

--- apps/enterprise/src/pages/proposal/WasmMsgPanel.tsx
+++ apps/enterprise/src/pages/proposal/WasmMsgPanel.tsx
@@ -1,17 +1,19 @@
+import { useState } from 'react';
 import { Container } from '@terra-money/apps/components';
 import { Panel } from 'components/panel';
 import { Button, Text } from 'components/primitives';
 import { useClipboardCopy } from 'hooks';
 import { encodedWasmFields, ExecuteMsgInput } from 'pages/create-proposal/execute/helpers/toExecuteMsg';
 import styles from './WasmMsgPanel.module.sass';
+import { HStack } from 'lib/ui/Stack';
 
 export type WasmMsgPanelProps = {
   msg: string;
 };
 
-const formatMsg = (value: string) => {
+const formatMsg = (value: string, showDecoded: boolean) => {
   const result: ExecuteMsgInput = JSON.parse(value);
-  if (result.wasm) {
+  if (result.wasm && showDecoded) {
     encodedWasmFields.forEach((fieldName) => {
       const field = result.wasm?.[fieldName];
       if (field) {
@@ -19,7 +21,6 @@ const formatMsg = (value: string) => {
       }
     });
   }
-
   return result;
 };
 
@@ -29,19 +30,32 @@ const fromBase64 = (value: string) => {
 
 export const WasmMsgPanel = ({ msg }: WasmMsgPanelProps) => {
   const copy = useClipboardCopy();
+  const [showDecoded, setShowDecoded] = useState(false);
+
+  const toggleDecoded = () => {
+    setShowDecoded(!showDecoded);
+  };
 
   return (
     <Panel className={styles.root}>
       <Container className={styles.top} direction="row">
         <Text variant="label">Wasm message</Text>
-        <Button
-          variant="secondary"
-          onClick={() => copy({ value: JSON.stringify(msg, null, 2), message: 'Message copied to clipboard' })}
-        >
-          Copy
-        </Button>
+        <HStack>
+          <Button
+            variant="secondary"
+            onClick={() => copy({ value: JSON.stringify(msg, null, 2), message: 'Message copied to clipboard' })}
+          >
+            Copy
+          </Button>
+          <Button variant="secondary" onClick={toggleDecoded} className={styles.showDecoded}>
+            {showDecoded ? 'Show Base64' : 'Show Decoded'}
+          </Button>
+        </HStack>
+
       </Container>
-      <pre className={styles.message}>{JSON.stringify(formatMsg(msg), null, 4)}</pre>
+      <pre className={styles.message}>
+        {JSON.stringify(formatMsg(msg, showDecoded), null, 4)}
+      </pre>
     </Panel>
   );
 };
