8c5aa30118b8fa1ac311f5941598a312ef276abe
Rodion Chachura <geekrodion@gmail.com>
2023-04-20 18:56:29 +0400

    feat: token info and membership review

--- apps/enterprise/src/pages/create-dao/review/MembershipReview.tsx
+++ apps/enterprise/src/pages/create-dao/review/MembershipReview.tsx
@@ -1,5 +1,22 @@
+import { LabeledValue } from "lib/ui/LabeledValue";
+import { useDaoWizardForm } from "../DaoWizardFormProvider";
+
 export const MembershipReview = () => {
+  const {
+    formState: { nftMembership },
+  } = useDaoWizardForm();
+
   return (
-    <p>Coming soon</p>
+    <>
+      <LabeledValue name="Minter">
+        {nftMembership.minter || '-'}
+      </LabeledValue>
+      <LabeledValue name="NFT Name">
+        {nftMembership.nftName || '-'}
+      </LabeledValue>
+      <LabeledValue name="NFT Symbol">
+        {nftMembership.nftSymbol || '-'}
+      </LabeledValue>
+    </>
   );
 }
