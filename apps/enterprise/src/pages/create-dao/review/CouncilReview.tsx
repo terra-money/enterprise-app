8911aed71ca570a37cc6c96105727bf6afd23660
Rodion Chachura <geekrodion@gmail.com>
2023-04-20 13:23:35 +0400

    feat: list council members

--- apps/enterprise/src/pages/create-dao/review/CouncilReview.tsx
+++ apps/enterprise/src/pages/create-dao/review/CouncilReview.tsx
@@ -1,5 +1,28 @@
+import { VStack } from "lib/ui/Stack";
+import { useDaoWizardForm } from "../DaoWizardFormProvider";
+import { Text } from "lib/ui/Text";
+
 export const CouncilReview = () => {
+  const { formState: { council } } = useDaoWizardForm();
+
+  if (!council?.members.length) {
+    return <Text>No council</Text>
+  }
+
+  const { members } = council;
+
   return (
-    <p>Coming soon</p>
+    <VStack gap={4}>
+      <VStack>
+        <Text weight="bold">Members:</Text>
+        {members.map((member, index) => {
+          return (
+            <Text>
+              {index + 1}. {member.address}
+            </Text>
+          )
+        })}
+      </VStack>
+    </VStack>
   );
 }
