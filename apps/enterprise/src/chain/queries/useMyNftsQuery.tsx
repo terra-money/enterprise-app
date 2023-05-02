6b7ed1603bc75893ae5f24d076adf894e0cbe9c8
Thomas Le <tvle83@gmail.com>
2023-05-02 00:17:07 -0500

    Updated the fetchNftIds function to use a do...while loop instead of recursion. more info on issue #71

--- apps/enterprise/src/chain/queries/useMyNftsQuery.tsx
+++ apps/enterprise/src/chain/queries/useMyNftsQuery.tsx
@@ -1,13 +1,14 @@
-import { useAssertMyAddress } from 'chain/hooks/useAssertMyAddress';
-import { useContract } from 'chain/hooks/useContract';
-import { QUERY_KEY } from 'queries';
-import { useQuery } from 'react-query';
-import { useQueryNftInfo } from './useNftInfoQuery';
+import {useAssertMyAddress} from 'chain/hooks/useAssertMyAddress';
+import {useContract}        from 'chain/hooks/useContract';
+import {QUERY_KEY}          from 'queries';
+import {useQuery}           from 'react-query';
+import {useQueryNftInfo}    from './useNftInfoQuery';
 
 interface MyNftIdsParams {
   tokens: {
     owner: string;
     start_after?: string;
+    limit: number;
   };
 }
 
@@ -18,24 +19,28 @@ type MyNftIdsResponse = {
 };
 
 export const useMyNftsQuery = (collectionAddr: string) => {
-  const { query } = useContract();
+  const {query} = useContract();
 
   const queryNftInfo = useQueryNftInfo();
-
-  const address = useAssertMyAddress();
+  const address      = useAssertMyAddress();
 
   const fetchNftIds = async (startAfter?: string): Promise<string[]> => {
-    const { ids, tokens } = await query<MyNftIdsParams, MyNftIdsResponse>(collectionAddr, {
-      tokens: { owner: address, start_after: startAfter },
-    });
+    let allIds: string[] = [];
 
-    if (!tokens || tokens.length === 0) {
-      return ids || [];
-    }
+    do {
+      const {ids, tokens} = await query<MyNftIdsParams, MyNftIdsResponse>(collectionAddr, {
+        tokens: {owner: address, start_after: startAfter, limit: 30}
+      });
 
-    const lastId = tokens[tokens.length - 1];
-    const nextIds = await fetchNftIds(lastId);
-    return [...(ids || []), ...nextIds];
+      if (tokens && tokens.length > 0) {
+        allIds     = [...allIds, ...tokens];
+        startAfter = tokens[tokens.length - 1];
+      } else {
+        startAfter = undefined;
+      }
+    } while (startAfter);
+
+    return allIds;
   };
 
   return useQuery([QUERY_KEY.MY_NFTS, collectionAddr], async () => {
