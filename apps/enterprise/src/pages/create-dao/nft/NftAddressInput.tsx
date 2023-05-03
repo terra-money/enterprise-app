3165078b80dca529a6eea0593bba9d2471f7c9fd
evanorti <87997759+evanorti@users.noreply.github.com>
2023-05-03 16:02:03 -0700

    Update NftAddressInput.tsx

--- apps/enterprise/src/pages/create-dao/nft/NftAddressInput.tsx
+++ apps/enterprise/src/pages/create-dao/nft/NftAddressInput.tsx
@@ -9,8 +9,13 @@ export const NftAddressInput = () => {
 
   return (
     <WizardInput
+<<<<<<< Updated upstream
       label="NFT address"
       placeholder="Type your existing CW721 NFT address"
+=======
+      label="NFT contract address"
+      placeholder="Type your existing CW721 NFT contract address"
+>>>>>>> Stashed changes
       value={existingNFTAddr}
       error={existingNFTError}
       valid={existingNFT !== undefined}
