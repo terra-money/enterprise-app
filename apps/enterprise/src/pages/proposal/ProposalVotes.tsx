5ebf5c3a3d958fb49f94d66398fdd360838b3fa3
Rodion Chachura <geekrodion@gmail.com>
2023-05-19 13:23:28 +0400

    fix: proposal votes

--- apps/enterprise/src/pages/proposal/ProposalVotes.tsx
+++ apps/enterprise/src/pages/proposal/ProposalVotes.tsx
@@ -10,10 +10,32 @@ import { toPercents } from 'lib/shared/utils/toPercents';
 import { useIsSmallScreen } from 'lib/ui/hooks/useIsSmallScreen';
 import { LabeledPageSection } from 'lib/ui/LabeledPageSection';
 import { Panel } from 'lib/ui/Panel/Panel';
-import { HStack, VStack } from 'lib/ui/Stack';
+import { VStack } from 'lib/ui/Stack';
 import { useCW20TokenInfoQuery, useTokenStakingAmountQuery } from 'queries';
 import { useProposalVotesQuery } from 'queries/useProposalVotesQuery';
 import { useCurrentProposal } from './CurrentProposalProvider';
+import styled from 'styled-components';
+
+const Content = styled.div`
+  display: grid;
+  grid-template-columns: 80px auto 80px 200px;
+
+  > * {
+    :last-child {
+      justify-self: end;
+    }
+  }
+
+  @media (max-width: 800px) {
+    grid-template-columns: auto auto;
+
+    > * {
+    :last-child {
+      justify-self: start;
+    }
+  }
+  }
+`
 
 // TODO: display date when contracts provide it
 export const ProposalVotes = () => {
@@ -44,16 +66,16 @@ export const ProposalVotes = () => {
         <PaginatedView onRequestToLoadMore={fetchNextPage} isLoading={isLoading || isFetchingNextPage}>
           {votes?.map(({ outcome, amount, voter }, index) => (
             <Panel key={index}>
-              <HStack justifyContent="space-between" alignItems="center">
+              <Content>
                 <Text variant="heading4">{capitalizeFirstLetter(outcome)}</Text>
                 <Address truncation={isSmallScreen ? [7, 4] : undefined} address={voter} />
                 {totalAvailableVotes.gt(0) && (
-                  <Text variant="text">{toPercents(amount.div(totalAvailableVotes).toNumber(), undefined , 3)}</Text>
+                  <Text variant="text">{toPercents(amount.div(totalAvailableVotes).toNumber(), undefined, 3)}</Text>
                 )}
                 {proposal.type !== 'council' && token && (
                   <Text variant="text">{`${demicrofy(amount, token.decimals ?? 6)} ${token.symbol}`}</Text>
                 )}
-              </HStack>
+              </Content>
             </Panel>
           ))}
         </PaginatedView>
