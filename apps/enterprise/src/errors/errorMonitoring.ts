0f260ec9a7aa84995fd1d78d851f0afda7ef24b3
Rodion Chachura <geekrodion@gmail.com>
2023-05-22 11:20:48 +0400

    feat: Sentry

--- apps/enterprise/src/errors/errorMonitoring.ts
+++ apps/enterprise/src/errors/errorMonitoring.ts
@@ -1,4 +1,43 @@
-// TODO: integrate with Sentry or similar
+import { ExtraErrorData as ExtraErrorDataIntegration } from '@sentry/integrations'
+import * as Sentry from '@sentry/react'
+import { isProduction } from 'shared'
+import { assertEnvVar } from 'shared/assertEnvVar'
+
+export const setupErrorMonitoring = () => {
+  if (!isProduction) return
+
+  Sentry.init({
+    ignoreErrors: [
+      'ResizeObserver loop limit exceeded',
+      'ResizeObserver loop completed with undelivered notifications.',
+      '$ is not defined',
+      'window.fetch is not a function',
+    ],
+    dsn: assertEnvVar('SENTRY_KEY'),
+    integrations: [new ExtraErrorDataIntegration({ depth: 10 })],
+    tracesSampleRate: 1.0,
+  })
+
+  Sentry.configureScope((scope) =>
+    scope.setTag('version', assertEnvVar('VERSION'))
+  )
+}
+
+export const setUserIdForErrorMonitoring = (userId: string) => {
+  if (!isProduction) return
+
+  Sentry.configureScope((scope) => {
+    scope.setUser({ id: userId })
+  })
+}
+
 export const reportError = (error: any, extra: Record<string, string> = {}) => {
-  console.info('reporting an error', error, extra);
-};
+  console.log('reportError', error, extra)
+  Sentry.withScope((scope) => {
+    Object.entries(extra).forEach(([key, value]) => {
+      scope.setExtra(key, value)
+    })
+    Sentry.captureException(error)
+  })
+}
+
---
+++ apps/enterprise/src/shared/assertEnvVar.ts
@@ -0,0 +1,13 @@
+type VariableName =
+  | 'SENTRY_KEY'
+  | 'VERSION'
+
+export const assertEnvVar = (name: VariableName): string => {
+  const envVarName = `REACT_APP_${name}`
+  const value = process.env[envVarName]
+  if (!value) {
+    throw new Error(`Missing ${envVarName} environment variable`)
+  }
+
+  return value
+}
