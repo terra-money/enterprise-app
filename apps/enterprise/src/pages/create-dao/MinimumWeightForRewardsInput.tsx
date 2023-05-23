5ba183915245220a9e06bee07eff251c74892f97
evanorti <87997759+evanorti@users.noreply.github.com>
2023-05-22 17:37:19 -0700

    Update MinimumWeightForRewardsInput.tsx

--- apps/enterprise/src/pages/create-dao/MinimumWeightForRewardsInput.tsx
+++ apps/enterprise/src/pages/create-dao/MinimumWeightForRewardsInput.tsx
@@ -11,7 +11,7 @@ export const MinimumWeightForRewardsInput = ({ value, onChange, error }: Minimum
   return (
     <FormControl
       label="Minimum weight for rewards"
-      helpText="The minimum weight that a DAO member should have in order to qualify for rewards. E.g. a value of 3 here means that a user in a token or NFT DAO needs at least 3 staked DAO assets, or a weight of 3 in multisig DAO, to be eligible for rewards."
+      helpText="The minimum weight a DAO member needs in order to qualify for rewards. For example, a value of 3 means that a user in a token or NFT DAO needs at least 3 voting shares to receive rewards"
     >
       <FormTextInput
         value={value === undefined ? '' : value}
