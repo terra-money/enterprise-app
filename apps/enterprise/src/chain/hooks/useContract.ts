85816243647504d2387632375836697c2ef5e67d
Rodion Chachura <geekrodion@gmail.com>
2023-05-25 15:51:27 +0400

    remove unecessary reporting

--- apps/enterprise/src/chain/hooks/useContract.ts
+++ apps/enterprise/src/chain/hooks/useContract.ts
@@ -1,18 +1,12 @@
 import { useLCDClient } from '@terra-money/wallet-provider';
 import { useCallback } from 'react';
-import { reportError } from 'errors/errorMonitoring';
 
 export const useContract = () => {
   const lcd = useLCDClient();
 
   const query = useCallback(
     async <QueryMsg extends {}, QueryResponse>(contractAddress: string, message: QueryMsg): Promise<QueryResponse> => {
-      try {
-        return await lcd.wasm.contractQuery<QueryResponse>(contractAddress, message);
-      } catch (err) {
-        reportError(err, { contractAddress, message: JSON.stringify(message) });
-        throw err;
-      }
+      return lcd.wasm.contractQuery<QueryResponse>(contractAddress, message);
     },
     [lcd.wasm]
   );
