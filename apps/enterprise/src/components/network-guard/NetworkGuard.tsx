9f4757b39de81514345b762717e1b9a2a03f58b2
Rodion Chachura <geekrodion@gmail.com>
2023-05-30 16:24:27 +0400

    fix: reload page if wrong network

--- apps/enterprise/src/components/network-guard/NetworkGuard.tsx
+++ apps/enterprise/src/components/network-guard/NetworkGuard.tsx
@@ -4,6 +4,8 @@ import { useEffect } from 'react';
 import { supportedChains, useChainID } from '@terra-money/apps/hooks';
 import { Center } from 'lib/ui/Center';
 import { Text } from 'lib/ui/Text';
+import { useConnectedWallet } from "@terra-money/wallet-provider"
+import { Spinner } from 'lib/ui/Spinner';
 
 export const NetworkGuard = (props: UIElementProps) => {
   const { children } = props;
@@ -12,9 +14,26 @@ export const NetworkGuard = (props: UIElementProps) => {
 
   const chainID = useChainID();
 
+  // TODO: This is a hack to fix the issue of the wallet not updating when the network changes.
+  const connectedWallet = useConnectedWallet()
+  const hasInconsistency = connectedWallet && (!connectedWallet.addresses[chainID] || !connectedWallet.network[chainID])
+  useEffect(() => {
+    if (hasInconsistency) {
+      window.location.reload()
+    }
+  })
+
   useEffect(() => {
     queryClient.invalidateQueries();
   }, [chainID, queryClient]);
+
+  if (hasInconsistency) {
+    return (
+      <Center>
+        <Spinner />
+      </Center>
+    );
+  }
 
   if (!supportedChains.includes(chainID)) {
     return (
@@ -26,3 +45,4 @@ export const NetworkGuard = (props: UIElementProps) => {
 
   return <>{children}</>;
 };
+
