dcfe206aa66ce250145fc5797579350d79613c38
emidev98 <emilian@terra.money>
2023-05-30 11:41:41 +0300

    fix: useChainID when user switch network

--- apps/enterprise/src/chain/hooks/useMyAddress.tsx
+++ apps/enterprise/src/chain/hooks/useMyAddress.tsx
@@ -4,6 +4,5 @@ import { useConnectedWallet } from '@terra-money/wallet-provider';
 export const useMyAddress = () => {
   const connectedWallet = useConnectedWallet();
   const chainID = useChainID();
-
   return connectedWallet?.addresses[chainID];
 };
