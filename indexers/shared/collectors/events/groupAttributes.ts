bd60f3160005b41945d7fdce3dc7095ae16b4a4b
tuky191 <michal@terra.money>
2023-06-15 11:07:50 +0200

    rework of groupAttributes

--- indexers/shared/collectors/events/groupAttributes.ts
+++ indexers/shared/collectors/events/groupAttributes.ts
@@ -20,21 +20,31 @@ export const groupAttributes = (attributes: Array<Attribute>): Array<Array<Attri
     groups.push(currentGroup);
   }
 
-  // Step 2: Merge groups that have the same _contract_address
+  // Step 2: Merge groups that have the same _contract_address and where one of the groups does not have an action key defined
   let result: Array<Array<Attribute>> = [];
   let groupMap: { [key: string]: Array<Attribute> } = {};
-
+  console.log(groups);
   groups.forEach((group) => {
     let contractAddress = group.find((attribute) => attribute.key === '_contract_address')?.value;
+    let hasActionKey = group.some((attribute) => attribute.key === 'action');
     if (contractAddress) {
-      if (!groupMap[contractAddress]) {
-        groupMap[contractAddress] = [];
+      let groupKey = `${contractAddress}`;
+      if (!groupMap[groupKey]) {
+        groupMap[groupKey] = [];
       }
-      group.forEach((attribute) => {
-        if (attribute.key !== '_contract_address' || groupMap[contractAddress].length === 0) {
-          groupMap[contractAddress].push(attribute);
-        }
-      });
+      if (
+        !hasActionKey ||
+        groupMap[groupKey].length === 0 ||
+        !groupMap[groupKey].some((attribute) => attribute.key === 'action')
+      ) {
+        group.forEach((attribute) => {
+          if (attribute.key !== '_contract_address' || groupMap[groupKey].length === 0) {
+            groupMap[groupKey].push(attribute);
+          }
+        });
+      } else {
+        result.push(group);
+      }
     }
   });
 
