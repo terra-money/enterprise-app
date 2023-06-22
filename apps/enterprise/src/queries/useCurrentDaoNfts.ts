0e747aee7c88cd46016c3c4390cd56b5af34bc20
Rodion Chachura <geekrodion@gmail.com>
2023-06-22 18:51:40 +0400

    feat: use collection floor price

--- apps/enterprise/src/queries/useCurrentDaoNfts.ts
+++ apps/enterprise/src/queries/useCurrentDaoNfts.ts
@@ -2,13 +2,13 @@ import { useCurrentDao } from "dao/components/CurrentDaoProvider";
 import { useCurrentDaoNftWhitelistQuery } from "./useCurrentDaoNftWhitelistQuery";
 import { useQuery } from "react-query";
 import { QUERY_KEY } from "./queryKey";
-import { NftInfoWithPrice } from "chain/Nft";
+import { NftWithPrice } from "chain/Nft";
 import { getNfts } from "chain/utils/getNfts";
 import { useContract } from "chain/hooks/useContract";
 import { assertDefined } from "@terra-money/apps/utils";
 import { usePricesOfLiquidAssets } from "chain/queries/usePricesOfLiquidAssets";
-import { getNftInfo } from "chain/utils/getNftInfo";
 import { useCurrentDaoStakedNfts } from "./useCurrentDaoStakedNfts";
+import { getCollectionInfo } from "chain/utils/getCollectionInfo";
 
 export const useCurrentDaoNfts = () => {
   const { address, dao_type, dao_membership_contract } = useCurrentDao();
@@ -21,31 +21,28 @@ export const useCurrentDaoNfts = () => {
 
   return useQuery([QUERY_KEY.DAO_NFTS, address], async () => {
     return (await Promise.all(assertDefined(whitelist).map(async collection => {
-      let nfts = (await getNfts({ collection, owner: address, query }))
+      let nfts: NftWithPrice[] = (await getNfts({ collection, owner: address, query }))
 
       if (dao_type === 'nft' && collection === dao_membership_contract) {
         const stakedNftsSet = new Set(assertDefined(stakedNfts))
         nfts = nfts.filter(({ id }) => !stakedNftsSet.has(id))
       }
 
-      return Promise.all(nfts.map(async ({ collection, id }) => {
-        let nft: NftInfoWithPrice = {
-          collection,
-          id,
-          usd: undefined,
-          name: undefined,
-          image: undefined
+      try {
+        const info = await getCollectionInfo(collection)
+        if (info.floorPriceInLuna) {
+          const lunaPrice = assertDefined(liquidAssetsPrices)['uluna']
+          const usd = info.floorPriceInLuna * lunaPrice
+          nfts = nfts.map(nft => ({
+            ...nft,
+            usd
+          }))
         }
+      } catch (err) {
+        console.log(err)
+      }
 
-        try {
-          const info = await getNftInfo(nft)
-          nft = { ...nft, ...info }
-        } catch {
-          console.error(`Failed to get info for an NFT collection=${collection} id=${id}`);
-        }
-
-        return nft
-      }))
+      return nfts
     }))).flat()
   }, {
     enabled: Boolean(whitelist && liquidAssetsPrices && stakedNfts)
