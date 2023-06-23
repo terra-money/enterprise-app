1d38aca7f911873a3e4145c15ecc8a35176e133d
Rodion Chachura <geekrodion@gmail.com>
2023-06-23 16:40:30 +0400

    update: query for nft info

--- apps/enterprise/src/dao/components/TreasuryNfts/NftItem.tsx
+++ apps/enterprise/src/dao/components/TreasuryNfts/NftItem.tsx
@@ -1,14 +1,17 @@
 import { formatAmount } from "@terra-money/apps/libs/formatting";
-import { NftInfoWithPrice } from "chain/Nft";
+import { NftWithPrice } from "chain/Nft";
+import { getNftInfo } from "chain/utils/getNftInfo";
 import { Panel } from "lib/ui/Panel/Panel";
 import { SafeImage } from "lib/ui/SafeImage";
 import { VStack } from "lib/ui/Stack";
 import { Text } from "lib/ui/Text";
 import { CoverImage } from "lib/ui/images/CoverImage";
 import { getColor } from "lib/ui/theme/getters";
+import { QUERY_KEY } from "queries/queryKey";
+import { useQuery } from "react-query";
 import styled from "styled-components";
 
-interface NftItemProps extends NftInfoWithPrice { }
+interface NftItemProps extends NftWithPrice { }
 
 const Container = styled(Panel)`
   padding: 0;
@@ -24,18 +27,25 @@ const ImageWr = styled.div`
   background: ${getColor('backgroundGlass')};
 `
 
-export const NftItem = ({ collection, id, image, usd, name }: NftItemProps) => {
+export const NftItem = ({ usd, ...nft }: NftItemProps) => {
+  const { data: info, isLoading, isError } = useQuery(
+    [QUERY_KEY.NFT_INFO, nft],
+    async () => {
+      return getNftInfo(nft)
+    }
+  );
+
   return (
     <Container>
       <ImageWr>
         <SafeImage
-          src={image}
+          src={info?.image}
           render={(props) => <CoverImage {...props} />}
         />
       </ImageWr>
       <Info>
         <VStack gap={16}>
-          <Text cropped weight='semibold' color="supporting">{name}</Text>
+          <Text cropped weight='semibold' color="supporting">{isLoading ? 'Loading...' : isError ? 'Failed to load' : info?.name}</Text>
           <Text weight="semibold" size={20}>
             ${formatAmount(usd || 0)}
           </Text>
