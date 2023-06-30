90ed102443ca26195b511935dd81a7bcc84ea0b6
Vlad <vladjdk@gmail.com>
2023-06-30 02:25:20 -0400

    Fix: Don't display "Show Less" unless more items are available

--- apps/enterprise/src/dao/components/TreasuryPanel/index.tsx
+++ apps/enterprise/src/dao/components/TreasuryPanel/index.tsx
@@ -99,7 +99,7 @@ export function TreasuryPanel<T>({ icon, itemName, items, isError, getTotalUsdVa
                 {renderFooterMsg()}
               </Text>
               <Actions childrenWidth={186} gap={16}>
-                {items && (
+                {items && itemsToDisplay.length > 4 && (
                   <PrimaryButton kind="secondary" onClick={() => setShouldShowAllItems(!shouldShowAllItems)}>
                     {itemsToDisplay.length < items.length ? 'Show more' : 'Show less'}
                   </PrimaryButton>
