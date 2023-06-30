dcfd5b3ae7c06f679d97aa6e063c65feb383385b
Vlad <vladjdk@gmail.com>
2023-06-30 02:00:13 -0400

    Chore: remove beta guard

--- apps/enterprise/src/navigation/components/AppRoutesWrapper.tsx
+++ apps/enterprise/src/navigation/components/AppRoutesWrapper.tsx
@@ -1,13 +1,10 @@
 import { Navigation } from 'components/Navigation';
-import { BetaGuard } from 'components/beta-guard';
 import { InitizalizedWalletOnly } from 'components/conditional-wallet/InitializedWalletOnly';
 
 export const AppRoutesWrapper = () => {
   return (
     <InitizalizedWalletOnly>
-      <BetaGuard>
         <Navigation />
-      </BetaGuard>
     </InitizalizedWalletOnly>
   );
 };
