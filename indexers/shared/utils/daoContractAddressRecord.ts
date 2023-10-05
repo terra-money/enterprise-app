f38997e25a5ee4d9144459e4f5e817082a4fa642
Rodion Chachura <geekrodion@gmail.com>
2023-10-05 11:39:45 +0400

    update: addresses

--- indexers/shared/utils/daoContractAddressRecord.ts
+++ indexers/shared/utils/daoContractAddressRecord.ts
@@ -5,11 +5,11 @@ export type DaoContract = 'enterprise-factory' | 'enterprise-facade' | 'enterpri
 export const daoContractAddressRecord: Record<DaoContract, Record<NetworkName, string>> = {
   'enterprise-factory': {
     mainnet: 'terra1y2dwydnnnctdwwmvs23ct60fj626t66qk53cae2gc55k3ce92jmqldj0sf',
-    testnet: 'terra1rafjglr2gl7l66dmjmvc2r2lct57dpdfzvjfpdxjy5mk5gfk38mssegvy9',
+    testnet: 'terra16vhaar9vhysnr8y5qrs20gecactkc6cxdehzzf487rh60eqst25q3wk77r',
   },
   'enterprise-facade': {
     mainnet: '',
-    testnet: 'terra1xjnxvwrk8vj4ju6rclgy5sm47a0ll8lux54jdjyhntzgwycmeuyq5wvvm5',
+    testnet: 'terra1v5tw0fq7hdnjh2pg7xxxnm98n2mk56h0qnc3zk68s9s7t74yja0s3wgurm',
   },
   'enterprise-versioning': {
     mainnet: '',
